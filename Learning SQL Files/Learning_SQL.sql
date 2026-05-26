1c0c7d5fd74baaddabcf749b8705a3239f445ce4
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-05-26 08:56:32 +0530

    Add SQL commands for database and table creation
    Added SQL commands to create a database and table structure.

--- Learning SQL Files/Learning_SQL.sql
+++ Learning SQL Files/Learning_SQL.sql
@@ -1,1 +1,14 @@
 
+-- CREATE Database CRX_LTD;
+
+use crx_ltd ;
+
+-- CREATE Table Employees (
+--       emp_id int,
+--       name varchar(50),
+--       age int,
+--       salary int
+-- );
+      
+Select * FROM employees;      
+
