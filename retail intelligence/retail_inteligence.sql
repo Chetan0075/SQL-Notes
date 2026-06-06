ac354451d22a5866285f28c1c3f30e185d1a6641
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-06-06 20:32:55 +0530

    project working file

---
+++ retail intelligence/retail_inteligence.sql
@@ -0,0 +1,62 @@
+-- CREATE DATABASE retail_intelligence ;
+
+USE retail_intelligence;
+
+-- Create table customers(
+-- customer_id INT PRIMARY KEY,
+-- name VARCHAR(100) NOT NULL,
+-- city VARCHAR(50) NOT NULL,
+-- email VARCHAR (100) UNIQUE,
+-- join_date DATE NOT NULL
+-- );
+
+-- INSERT INTO customers (customer_id, name, city, email, join_date)
+-- VALUES
+-- (1, 'chetan rajput', 'ralegaon', 'chetansuraswal07@gmail.com', '2026-07-07'),
+-- (2, 'Riya Patil', 'Mumbai', 'riya@gmail.com', '2024-02-15'),
+-- (3, 'Siddharth Jain', 'Delhi', 'siddharth@gmail.com', '2024-03-05'),
+-- (4, 'Neha Verma', 'Bangalore', 'neha@gmail.com', '2024-01-25'),
+-- (5, 'Karan Mehta', 'Hyderabad', 'karan@gmail.com', '2024-04-12'),
+-- (6, 'Pooja Singh', 'Pune', 'pooja@gmail.com', '2024-02-28'),
+-- (7, 'Rahul Nair', 'Chennai', 'rahul@gmail.com', '2024-03-18'),
+-- (8, 'Anjali Deshmukh', 'Mumbai', 'anjali@gmail.com', '2024-05-01'),
+-- (9, 'Vikram Rao', 'Delhi', 'vikram@gmail.com', '2024-04-22'),
+-- (10, 'Sneha Kulkarni', 'Pune', 'sneha@gmail.com', '2024-05-10');
+
+-- SELECT * FROM customers ;
+
+-- CREATE TABLE Products(
+-- product_id int primary key,
+-- product_name varchar(100) not null,
+-- category varchar(50) not null,
+-- price decimal(10, 2) not null
+-- )
+
+
+-- insert into products(product_id, product_name, category, price)
+-- values
+-- (1, 'wireless mouse', 'electronics', 799.00),
+-- (2, 'Mechanical Keyboard', 'Electronics', 2499.00),
+-- (3, 'USB-C Cable', 'Electronics', 199.00),
+-- (4, 'Office Chair', 'Furniture', 5499.00),
+-- (5, 'Study Table', 'Furniture', 3999.00),
+-- (6, 'Notebook Set', 'Stationery', 299.00),
+-- (7, 'Gel Pen Pack', 'Stationery', 149.00),
+-- (8, 'Backpack', 'Accessories', 999.00),
+-- (9, 'Water Bottle', 'Accessories', 499.00),
+-- (10, 'LED Desk Lamp', 'Electronics', 1299.00);
+
+
+-- select * from products;
+
+
+
+
+
+
+
+
+
+
+
+
