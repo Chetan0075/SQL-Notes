e83ced33928bb0c8620f0e5be3a9fc6c4a8bdbbc
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-06-08 09:30:00 +0530

    Comment out SQL queries in retail_intelligence_analysis.sql
    Comment out several SQL queries for clarity.

--- retail intelligence/retail_inteligence_analysis.sql
+++ retail intelligence/retail_inteligence_analysis.sql
@@ -1,5 +1,9 @@
 use retail_intelligence;
 
 -- select * from customers;
+-- select name , city from customers ;
+-- select name from customers where city = 'Pune';
 
-select name , city from customers ;
+-- SELECT * FROM products WHERE price > 1000;
+-- SELECT * FROM products ORDER BY price DESC;
+
