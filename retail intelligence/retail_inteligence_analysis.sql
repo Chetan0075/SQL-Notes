f98f4c7c220fd9b04dca4bae3f3497ad59026a14
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-06-08 09:31:29 +0530

    Update retail_inteligence_analysis.sql

--- retail intelligence/retail_inteligence_analysis.sql
+++ retail intelligence/retail_inteligence_analysis.sql
@@ -6,4 +6,6 @@ use retail_intelligence;
 
 -- SELECT * FROM products WHERE price > 1000;
 -- SELECT * FROM products ORDER BY price DESC;
+-- SELECT * FROM products WHERE category = 'Electronics';
+-- SELECT * FROM customers WHERE join_date > '2024-03-01';
 
