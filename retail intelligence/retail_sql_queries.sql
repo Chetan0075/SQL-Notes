a46e0c706d972afcfa4d43b401ca4ff9d242a25d
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-06-10 23:04:56 +0530

    Add SQL index creation comments for clarity
    Added commented SQL index creation statements for orders and order_details.

--- retail intelligence/retail_sql_queries.sql
+++ retail intelligence/retail_sql_queries.sql
@@ -1,1 +1,33 @@
+-- CREATE INDEX idx_orders_customer
+-- ON orders(customer_id);
+
+-- CREATE INDEX idx_order_details_order
+-- ON order_details(order_id);
+
+-- CREATE INDEX idx_order_details_product
+-- ON order_details(product_id);
+
+-- CREATE INDEX idx_orders_date
+-- ON orders(order_date);
+
+
+-- SHOW INDEX FROM orders;
+-- SHOW INDEX FROM order_details;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
