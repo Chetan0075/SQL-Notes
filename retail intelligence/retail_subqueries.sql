5eb1b557f3a3e5a74879ed927664f3ac232eb51e
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-06-11 11:54:35 +0530

    Comment out SQL code in retail_subqueries.sql
    Comment out SQL queries for customer totals and product updates.

--- retail intelligence/retail_subqueries.sql
+++ retail intelligence/retail_subqueries.sql
@@ -125,3 +125,32 @@ use retail_intelligence;
 
 -- ROLLBACK;
 
+-- WITH customer_totals AS
+-- (
+--     SELECT
+--         c.customer_id,
+--         c.name,
+--         SUM(od.quantity * p.price) AS total_spent
+--     FROM customers c
+--     JOIN orders o ON c.customer_id = o.customer_id
+--     JOIN order_details od ON o.order_id = od.order_id
+--     JOIN products p ON od.product_id = p.product_id
+--     GROUP BY c.customer_id, c.name
+-- )
+
+-- SELECT *
+-- FROM customer_totals
+-- WHERE total_spent > 5000;
+
+
+-- START TRANSACTION;
+
+-- UPDATE products
+-- SET price = price + 100
+-- WHERE product_id = 1;
+
+-- ROLLBACK;
+
+-- SELECT *
+-- FROM products
+-- WHERE product_id = 1;
