f7f558760f0c85dafd51d38b24df704ad070cf77
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-06-11 11:59:50 +0530

    Comment out SQL queries in Business Insight Queries.sql
    Commented out SQL queries for business insights.

--- retail intelligence/Business Insight Queries.sql
+++ retail intelligence/Business Insight Queries.sql
@@ -8,3 +8,21 @@
 -- GROUP BY c.name
 -- ORDER BY revenue DESC
 -- LIMIT 5;
+
+-- SELECT
+--     p.product_name,
+--     SUM(od.quantity) AS total_sold
+-- FROM products p
+-- JOIN order_details od
+-- ON p.product_id = od.product_id
+-- GROUP BY p.product_name
+-- ORDER BY total_sold DESC;
+
+-- SELECT
+--     p.category,
+--     SUM(od.quantity * p.price) AS revenue
+-- FROM products p
+-- JOIN order_details od
+-- ON p.product_id = od.product_id
+-- GROUP BY p.category;
+
