d31afab0bd64fed7f51d8214ef47ae8114359659
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-06-11 11:58:46 +0530

    Add files via upload

---
+++ retail intelligence/Business Insight Queries.sql
@@ -0,0 +1,10 @@
+-- SELECT
+--     c.name,
+--     SUM(od.quantity * p.price) AS revenue
+-- FROM customers c
+-- JOIN orders o ON c.customer_id=o.customer_id
+-- JOIN order_details od ON o.order_id=od.order_id
+-- JOIN products p ON od.product_id=p.product_id
+-- GROUP BY c.name
+-- ORDER BY revenue DESC
+-- LIMIT 5;
