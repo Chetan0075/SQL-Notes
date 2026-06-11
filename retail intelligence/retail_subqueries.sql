601a90d11058edd9abfd694c8bf616d87339815c
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-06-11 10:15:56 +0530

    Add queries to calculate customer spending

--- retail intelligence/retail_subqueries.sql
+++ retail intelligence/retail_subqueries.sql
@@ -1,1 +1,45 @@
+use retail_intelligence;
+
+-- SELECT MAX(price)
+-- FROM products;
+
+-- SELECT *
+-- FROM products
+-- WHERE price = (
+--     SELECT MAX(price)
+--     FROM products
+-- );
+
+
+
+SELECT
+    c.customer_id,
+    SUM(od.quantity * p.price) AS total_spent
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+GROUP BY c.customer_id;
+
+
+SELECT *
+FROM customer_spending
+WHERE total_spent >
+(
+    SELECT AVG(total_spent)
+    FROM customer_spending
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
