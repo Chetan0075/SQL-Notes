a80bb0f7e6c0f6083227c6e92291069f6484a702
Chetan Suraswal <151898636+Chetan0075@users.noreply.github.com>
2026-06-11 10:18:36 +0530

    Comment out SQL queries in retail_subqueries.sql
    Comment out various SQL queries for future reference.

--- retail intelligence/retail_subqueries.sql
+++ retail intelligence/retail_subqueries.sql
@@ -1,42 +1,54 @@
 use retail_intelligence;
 
--- SELECT MAX(price)
--- FROM products;
+-- -- SELECT MAX(price)
+-- -- FROM products;
+
+-- -- SELECT *
+-- -- FROM products
+-- -- WHERE price = (
+-- --     SELECT MAX(price)
+-- --     FROM products
+-- -- );
+
+
+
+-- SELECT
+--     c.customer_id,
+--     SUM(od.quantity * p.price) AS total_spent
+-- FROM customers c
+-- JOIN orders o ON c.customer_id = o.customer_id
+-- JOIN order_details od ON o.order_id = od.order_id
+-- JOIN products p ON od.product_id = p.product_id
+-- GROUP BY c.customer_id;
+
 
 -- SELECT *
--- FROM products
--- WHERE price = (
---     SELECT MAX(price)
---     FROM products
+-- FROM customer_spending
+-- WHERE total_spent >
+-- (
+--     SELECT AVG(total_spent)
+--     FROM customer_spending
 -- );
 
 
 
-SELECT
-    c.customer_id,
-    SUM(od.quantity * p.price) AS total_spent
-FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN order_details od ON o.order_id = od.order_id
-JOIN products p ON od.product_id = p.product_id
-GROUP BY c.customer_id;
-
-
-SELECT *
-FROM customer_spending
-WHERE total_spent >
-(
-    SELECT AVG(total_spent)
-    FROM customer_spending
-);
+-- SELECT *
+-- FROM customers
+-- WHERE customer_id IN
+-- (
+--     SELECT customer_id
+--     FROM orders
+-- );
 
 
 
-
-
-
-
-
+-- SELECT *
+-- FROM products
+-- WHERE product_id NOT IN
+-- (
+--     SELECT product_id
+--     FROM order_details
+-- );
 
 
 
